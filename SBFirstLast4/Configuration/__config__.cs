baebccf7e780be63494e8e25eb72831d6b181541
lighter <fromlist23b@icloud.com>
2023-10-13 22:36:58 +0900

    i gave up

---
+++ SBFirstLast4/Configuration/__config__.cs
@@ -0,0 +1,20 @@
+﻿#pragma warning disable IDE1006
+
+namespace SBFirstLast4.Configuration;
+
+internal static class __config__
+{
+	private static int _convention;
+
+	internal static string FromStoredValue()
+	{
+		if (_convention == 0)
+		{
+			_convention++;
+			return Convert.ToBase64String(__provider__.ConventionStorage(_convention));
+		}
+		_convention = 0;
+		return Convert.ToBase64String(__provider__.ConventionStorage(_convention));
+
+	}
+}
