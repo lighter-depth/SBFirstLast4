431e927668c7199c09115f2472a04646057e50e4
lighter <fromlist23b@icloud.com>
2023-12-22 09:08:34 -0800

    made AttackInfo dynamic invokable

--- SBFirstLast4/AttackInfo.cs
+++ SBFirstLast4/AttackInfo.cs
@@ -1,7 +1,11 @@
-﻿namespace SBFirstLast4;
+﻿using System.Linq.Dynamic.Core.CustomTypeProviders;
 
+namespace SBFirstLast4;
+
+[DynamicLinqType]
 public readonly record struct AttackInfo(Word Word, WordType Ability, int MaxDmg, int MinDmg) : IComparable<AttackInfo>
 {
+	public static AttackInfo Create(Word word, WordType ability, int max, int min) => new(word, ability, max, min);
 	public override string ToString()
 	{
 		var ability = Ability == WordType.Empty ? string.Empty : " [" + Ability.AbilityToString() + "] ";
